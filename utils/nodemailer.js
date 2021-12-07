7b5ecbbf7236d780e02f46beb3bf4423ab813f49
Mohit Daswani <daswani.mo@northeastern.edu>
2021-12-06 20:14:09 -0500

    resolved nodemailer

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,41 +1,40 @@
-const nodemailer=require('nodemailer')
-const dotenv=require('dotenv')
-dotenv.config()
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
 
-let transport=nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    port:465,
-    secure:true,
-    debug:process.env.NODE_ENV ==="develop",
-    auth:{
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASS
-    }
-})
+let transport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  debug: process.env.NODE_ENV === "develop",
+  auth: {
+    user: process.env.GMAIL_EMAIL,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
-
- let verify=async ()=>{
-    try{
-       let status = await transport.verify()
+let verify = async () => {
+  try {
+    let status = await transport.verify();
     //true is printing don't know why should be removed?????
-    } catch (err) {
-       console.log(err)                  
-    }
- }
- verify();
+    console.log(status);;;;
+  } catch (err) {
+    console.log(err);
+  }
+};
+verify();
 
- let send_mail=async (email,subject,html)=>{
-    let status = await transport.sendMail({
-       from:`ShubhKadam.com`, 
-       to:email,
-       subject:subject,
-       html:html
-    })
-    try {
-       console.log(status)                  
-    } catch (err) {
-       console.log(err)
-    }
- }
-
- module.exports = send_mail
+let send_mail = async (email, subject, html) => {
+  let status = await transport.sendMail({
+    from: `dummyprojectuser@gmail.com`,
+    to: email,
+    subject: subject,
+    html: html,
+  });
+  try {
+    console.log(status);
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = send_mail;
