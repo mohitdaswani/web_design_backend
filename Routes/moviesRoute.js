7bf318e22f5311aff3f933183dd9f73aec9a31af
Mohit Daswani <daswani.mo@northeastern.edu>
2021-12-07 17:56:59 -0500

    adding movie feature

---
+++ Routes/moviesRoute.js
@@ -0,0 +1,25 @@
+const { get, post, put, delete1 } = require("../controllers/moviesController");
+const { post1, get1 } = require("../controllers/watchListController");
+const { Router } = require("express");
+const authentication = require("../middlewares/authentication");
+const router = Router();
+router.get("/allMovies", authentication, get.getAllMovies);
+router.get(
+  "/movies/netflixOriginals",
+  authentication,
+  get.fetchNetflixOriginalMovies
+);
+router.get("/movies/topRated", authentication, get.fetchTopRatedMovies);
+router.get("/movies/TrendingMovies", authentication, get.fetchTrendingMovies);
+router.get("/movies/:genres", authentication, get.getMovieByGenre);
+router.get("/user/watchlist", authentication, get1.getUserWatchlist);
+router.get("/movie/:movieId", authentication, get.singleMovie);
+router.get("/watchList/:movieId", authentication, post1.addToWatchlist);
+router.get("/search", authentication, get.search_movie);
+router.get("/latest", authentication, get.fetchLatestMovies);
+router.get(
+  "/movies/language/:language",
+  authentication,
+  get.getMovieByLanguage
+);
+module.exports = router;
---
+++ models/movies.js
@@ -0,0 +1,27 @@
+let mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+let movieSchema = new Schema(
+  {
+    MovieName: { type: String },
+    title: { type: String },
+    description: { type: String },
+    language: { type: String },
+    posterImage: { type: Object },
+    backgroundImage: { type: Object },
+    movie: { type: Object },
+    isReleased: { type: Boolean, default: 0 },
+    country: { type: String },
+    isPaid: { type: Boolean },
+    releasedDate: { type: Date },
+    trending: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
+    runTime: { type: Number },
+    isAdult: { type: Boolean, default: 0 },
+    genre: { type: Object },
+  },
+  { timestamps: true }
+);
+const movies1 = mongoose.model("movies", movieSchema);
+
+module.exports = movies1;
