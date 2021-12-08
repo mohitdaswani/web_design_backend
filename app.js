b5fdbaf82cc2bbf545a11e5df1f347e68ef6e7ac
Mohit Daswani <daswani.mo@northeastern.edu>
2021-12-07 19:43:44 -0500

    updating links

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.use(
 
 const userRoute = require('./Routes/userRoute')
 const adminRoute = require("./Routes/adminRoute");
-// const movieRoute = require("./Routes/moviesRoute")
+const movieRoute = require("./Routes/moviesRoute")
 // const subscriptionRoute=require("./Routes/SubscriptionRoute")
 
 app.use(userRoute)
 app.use(adminRoute);
-// app.use(movieRoute)
+app.use(movieRoute)
 // app.use(subscriptionRoute)
 
 const handleSend = (req, res) => {
