6feed5279ea5e5cff74d914cadc76d4579f21820
Mohit Daswani <daswani.mo@northeastern.edu>
2021-12-07 19:04:30 -0500

    updating links

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,7 +64,7 @@ module.exports = {
           let subject = `Welcome to Movies`;
           let html = `<h2>Thanks for Joining Us</h2>
                         <h3>Dear ${name} you are one step closer to become one of our prestigious family</h3>
-                        <p>To verify your email Click <a href=http://powerful-tor-09724.herokuapp.com/user/verify/${token} >here</a></p>
+                        <p>http://powerful-tor-09724.herokuapp.com/user/verify/${token}</p>           
                                   <p>Thank you!!!!</p>`;
 
           email1(email, subject, html); //////////////////////function to send email to the user
