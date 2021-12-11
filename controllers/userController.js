62ceb58c00ac6f37337c26ccf00a1e16070e3509
Mohit Daswani <daswani.mo@northeastern.edu>
2021-12-10 21:18:43 -0500

    resolved Merge conflicts

--- controllers/userController.js
+++ controllers/userController.js
@@ -105,9 +105,9 @@ module.exports = {
             const resetToken = await users.generate_reset_token(user);
             let subject = `Password Reset`;
             let html = `<h2>Thanks for Joining Us</h2>
-                        <h3>Dear ${name} you are one step closer to become one of our prestigious family</h3>
-                        <p>To verify your email Click <a href=https://powerful-tor-09724.herokuapp.com/user/verify/${token} >here</a></p>
-                        <p>https://powerful-tor-09724.herokuapp.com/user/verify/${token}</p>           
+                        <h3>Dear ${user.name} you are one step closer to become one of our prestigious family</h3>
+                        <p>To verify your email Click <a href=https://powerful-tor-09724.herokuapp.com/user/verify/${resetToken} >here</a></p>
+                        <p>https://powerful-tor-09724.herokuapp.com/user/verify/${resetToken}</p>           
                                   <p>Thank you!!!!</p>`;
             await email1(user[0].email, subject, html);
             res.status(200).json({
